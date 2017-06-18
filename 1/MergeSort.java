bd78f3a6d06445dd9ecd818a7b3cf5506c27e331
lung-zijim <lzylong@gmail.com>
2017-06-18 15:34:45 -0400

    MergeSort

--- 1/MergeSort.java
+++ 1/MergeSort.java
@@ -15,12 +15,13 @@ public class MergeSort {
             return;                                          // return "nothing", mean recursion ends
         }
         int mid = left + (right - left) / 2 ;                // typical mid pointer solution
-        System.out.println(" left" + left + "Mid" + mid);
+        //System.out.println(" left" + left + "Mid" + mid);
         mergeSort(array, helper, left, mid);                 // sort left pat
-        System.out.println("mid+1 " + (mid + 1) + " right" + right);
+        //System.out.println("mid+1 " + (mid + 1) + " right" + right);
         mergeSort(array, helper, mid + 1, right);            // sort right part
-        System.out.println("merge L" + left + " M " + mid + " R " + right);
+        //System.out.println("merge L" + left + " M " + mid + " R " + right);
         merge(array, helper, left, mid, right);              // merge
+        //System.out.println(Arrays.toString(array));
     }
 
     private void  merge(int[] array, int[] helper, int left, int mid, int right) {
@@ -29,9 +30,10 @@ public class MergeSort {
         }
         int leftIndex = left;                                // left part start point
         int rightIndex = mid + 1;                            // right part start point
+        System.out.println(left + "---" + right);            // ------------------------output
         while (leftIndex <= mid && rightIndex <= right) {    // looping when left start point <= mid and
                                                              //              right start point <= right bound
-            System.out.println("leftIndex" + leftIndex + "rightIndex" + rightIndex + "mid" + mid + "left" + left + "right" + right);
+            //System.out.println("leftIndex" + leftIndex + "rightIndex" + rightIndex + "mid" + mid + "left" + left + "right" + right);
             if (helper[leftIndex] <= helper[rightIndex]) {   // right side is greater
                 array[left++] = helper[leftIndex++];         //  
             } else {
@@ -39,9 +41,11 @@ public class MergeSort {
             }
         }
         while (leftIndex <= mid) {
-            System.out.println("leftIndex" + leftIndex + "mid" + mid);
+            //System.out.println(Arrays.toString(array));
+            //System.out.println("leftIndex" + leftIndex + "mid" + mid);
             array[left++] = helper[leftIndex++];
         }
+        System.out.println(Arrays.toString(array));          // -----------------------outpout!
     }
 
     public static void main(String[] args) {
@@ -55,8 +59,8 @@ public class MergeSort {
         array = mg.mergeSort(array);
         System.out.println(Arrays.toString(array));
         **/
-        array = new int[] {4,3,2,1};
+        array = new int[] {4,2,1};
         array = mg.mergeSort(array);
-        System.out.println(Arrays.toString(array));
+        //System.out.println(Arrays.toString(array));
     }
 }
