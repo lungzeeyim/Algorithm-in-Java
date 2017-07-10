1b050bb18677401950b042de71b2a3be7ee468e2
lzylong@gmail.com <lzylong@gmail.com>
2017-07-10 18:33:55 -0400

    stack

---
+++ 3/QueueByTwoStacks.java
@@ -0,0 +1,14 @@
+public class QueueByTwoStacks {
+    // always insert into the in-stack
+    private LinkedList<Integer> in;
+
+    // always remove from the out-stack;
+    private LinkedList<Integer> out;
+
+    // Create a constructor!
+    public QueueByTwoStack() {
+        in = new LinkedList<Integer>();
+        out = new LinkedList<Integer>();
+    }
+    
+}
